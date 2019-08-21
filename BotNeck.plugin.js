67767b32f053d88ee61292da9aee649417b0f03e
AtiLion <AtiLion@users.noreply.github.com>
2019-08-21 20:11:43 +0200

    Began recode

--- BotNeck.plugin.js
+++ BotNeck.plugin.js
@@ -1,387 +1,158 @@
-//META{"name":"BotNeckPlugin"}*//
+//META{"name":"BotNeck","website":"https://github.com/AtiLion/BotNeck-Bot","source":"https://github.com/AtiLion/BotNeck-Bot/blob/master/BotNeck.plugin.js"}*//
 
-var BotNeckPlugin = function() {};
-var BotNeckAPI = function() {};
-var BotNeckModules = {};
-var BotNeckData = {
-	bdPath: "",
-	botneckModules: "",
-	botneckConfig: "",
-	eWindow: null,
-	Backup_Send: null,
-	LastMsgID: "",
-};
-var BotNeckConfig = {
-	commandPrefix: "->",
-};
-
-const { remote, BrowserWindow } = require('electron');
 const fs = require("fs");
+const path = require("path");
 
-BotNeckPlugin.config = "BotNeck";
+const sendRequest = XMLHttpRequest.prototype.send;
+const setRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
 
-BotNeckPlugin.prototype.onMessage = function() {};
-BotNeckPlugin.prototype.onSwitch = function() {};
-BotNeckPlugin.prototype.observer = function(e) {};
+const protectedObject = { // Protected, do not expose to modules
+	"token": null,
+	"usedKeys": [],
+	"lastUserMessageId": null,
+	"lastBotMessageId": null
+}
+const modules = {} // key: { permissions: [ "permission" ], sandbox: sandbox, module: module }
 
-BotNeckPlugin.prototype.getSettingsPanel = function()
-{
-	return "";
+/* Permissions:
+ * - authorized_request = Create a request with your Discord token attached
+*/
+
+function makeKey() {
+	var result           = '';
+	var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var charactersLength = characters.length;
+	for ( var i = 0; i < 16; i++ ) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	if(result in protectedObject["usedKeys"]) // Make sure there are no duplicates
+		return makeKey();
+	return result;
+}
+Function.prototype.clone = function() {
+    var that = this;
+    var temp = function temporary() { return that.apply(this, arguments); };
+    for(var key in this) {
+        if (this.hasOwnProperty(key)) {
+            temp[key] = this[key];
+        }
+    }
+    return temp;
 };
-BotNeckPlugin.prototype.getName = function()
-{
-    return "Bot Neck";
-};
-BotNeckPlugin.prototype.getDescription = function()
-{
-    return "A simple self-bot that runs as a plugin on your discord";
-};
-BotNeckPlugin.prototype.getVersion = function()
-{
-    return "0.1.0";
-};
-BotNeckPlugin.prototype.getAuthor = function()
-{
-    return "AtiLion";
-};
 
-BotNeckPlugin.prototype.start = function()
-{
-	BotNeckData.Backup_Send = XMLHttpRequest.prototype.send;
+class BotNeck {
+	getName() { return "BotNeck Bot"; }
+	getDescription() { return "Adds selfbot commands to the Discord client."; }
+	getVersion() { return "0.2.0"; }
+	getAuthor() { return "AtiLion"; }
 
-	(function(send) {
-		XMLHttpRequest.prototype.send = function(data) {
-			if(data != null)
-			{
-				let msg = null;
-				try
-				{
-					msg = JSON.parse(data);
-				}
-				catch(e)
-				{
-					send.call(this, data);
-					return;
-				}
+	load() {
+		// Get paths
+		this.modulesPath = path.join(window.ContentManager.pluginsFolder, "BotNeckModules");
+		this.botneckConfig = path.join(window.bdConfig.dataPath, "BotNeck.config.json");
 
-				if(!msg.hasOwnProperty("content") || !msg.hasOwnProperty("nonce") || !msg.hasOwnProperty("tts"))
-				{
-					send.call(this, data);
-					if(msg.hasOwnProperty("nonce") && msg.hasOwnProperty("tts"))
-					{
-						this.onload = function()
-						{
-							try
-							{
-								resp = JSON.parse(this.responseText);
-
-								if(resp.hasOwnProperty("id"))
-									BotNeckData.LastMsgID = resp.id;
-							}
-							catch (e)
-							{
-								// Meh don't worry about it
-							}
-						}
-					}
-					return;
-				}
-				if(typeof(msg.content) != "string")
-				{
-					send.call(this, JSON.stringify(msg));
-					return;
-				}
-				if(!msg.content.startsWith(BotNeckConfig.commandPrefix))
-				{
-					this.onload = function()
-					{
-						try
-						{
-							resp = JSON.parse(this.responseText);
-
-							if(resp.hasOwnProperty("id"))
-								BotNeckData.LastMsgID = resp.id;
-						}
-						catch (e)
-						{
-							// Meh don't worry about it
-						}
-					}
-					send.call(this, JSON.stringify(msg));
-					return;
-				}
-
-				let fullcmd = msg.content.substring(2);
-				if(fullcmd.startsWith(" "))
-					fullcmd = fullcmd.substring(1);
-				let spl = fullcmd.split(" ");
-				let command = spl[0];
-				let args = spl.slice(1);
-
-				try
-				{
-					if(BotNeckModules.hasOwnProperty(command))
-					{
-						if(typeof(BotNeckModules[command].minArgs) == "number")
-						{
-							if(BotNeckModules[command].minArgs > args.length)
-							{
-								let emb = {
-									title: "Bot Error",
-									type: "rich",
-									description: "Not enough arguments!",
-									color: 0xff6e00,
-								}
-
-								delete msg["content"];
-								msg["embed"] = emb;
-
-								send.call(this, JSON.stringify(msg));
-								return;
-							}
-						}
-						BotNeckModules[command].execute(msg, args);
-					}
-					else
-					{
-						let emb = {
-							title: "Bot Error",
-							type: "rich",
-							description: "No command " + command + " has been found!",
-							color: 0xff6e00,
-						}
-
-						delete msg["content"];
-						msg["embed"] = emb;
-					}
-				}
-				catch (e)
-				{
-					let emb = {
-						title: "Bot Error",
-						type: "rich",
-						description: "Error executing command " + command + "!",
-						color: 0xff6e00,
-					}
-
-					delete msg["content"];
-					msg["embed"] = emb;
-					BotNeckAPI.LogError("Error executing command " + command, e);
-				}
-				this.onload = function()
-				{
-					try
-					{
-						resp = JSON.parse(this.responseText);
-
-						if(resp.hasOwnProperty("id"))
-							BotNeckData.LastMsgID = resp.id;
-					}
-					catch (e)
-					{
-						// Meh don't worry about it
-					}
-				}
-				send.call(this, JSON.stringify(msg));
+		// Load modules
+		if(!fs.existsSync(this.modulesPath))
+			fs.mkdirSync(this.modulesPath);
+		fs.readdir(this.modulesPath, (err, files) => {
+			if(err) {
+				console.error("Error while getting modules!");
+				console.error(err);
 				return;
 			}
-			send.call(this, data);
-			return;
-		};
-	})(XMLHttpRequest.prototype.send);
-};
 
-BotNeckPlugin.prototype.stop = function()
-{
-	XMLHttpRequest.prototype.send = BotNeckData.Backup_Send;
-};
+			for(let filename of files) {
+				if(filename.indexOf(".botneck.js") < 0)
+					continue;
+				let file = path.join(this.modulesPath, filename);
+				let name = filename.slice(0, -(".botneck.js".length));
 
-BotNeckPlugin.prototype.load = function()
-{
-	BotNeckData.bdPath = (process.platform == 'win32' ? process.env.APPDATA : process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : '/var/local') + '/BetterDiscord/';
-	BotNeckData.botneckModules = BotNeckData.bdPath + "plugins/BotNeck_Modules"
-	BotNeckData.botneckConfig = BotNeckData.bdPath + "plugins/" + BotNeckPlugin.config + ".json";
-	BotNeckData.eWindow = remote.getCurrentWindow();
+				// Sandbox and execute
+				fs.readFile(file, (err, data) => {
+					if(err) {
+						console.error("Error while reading file", filename);
+						console.error(err);
+						return;
+					}
 
-	if(!fs.existsSync(BotNeckData.botneckModules))
-		fs.mkdirSync(BotNeckData.botneckModules);
-	BotNeckAPI.LoadModules();
-	BotNeckAPI.LoadConfig();
-};
+					let sandbox = new BotNeckSandBox();
+					sandbox.name = name;
 
-BotNeckPlugin.prototype.unload = function()
-{
-	BotNeckAPI.SaveConfig();
-	BotNeckAPI.UnloadModules();
-};
-// ------------------------------------------------------ API ------------------------------------------------------------------ //
+					sandbox.execute(data);
 
-BotNeckAPI.Log = function(msg)
-{
-	console.log("[BOTNECK] " + msg);
-};
-BotNeckAPI.LogError = function(msg, ex)
-{
-	console.error("[BOTNECK_ERROR] " + msg)
-	console.error(ex)
-};
-
-BotNeckAPI.LoadModules = function()
-{
-	fs.readdirSync(BotNeckData.botneckModules).forEach(function(filename)
-	{
-		if(!filename.endsWith(".module.js"))
-			return;
-
-		let module = fs.readFileSync(BotNeckData.botneckModules + "/" + filename, "utf-8");
-		let name = filename.slice(0, -(".module.js".length));
-
-		try
-		{
-			eval(module);
-			eval("(function() { let mod = new " + name + "(); BotNeckModules[mod.command] = mod; BotNeckAPI.Log(mod.command + ' loaded!'); })();");
-		}
-		catch (e)
-		{
-			BotNeckAPI.LogError("Unable to load module: " + name, e);
-		}
-	});
-};
-BotNeckAPI.UnloadModules = function()
-{
-	BotNeckModules = {};
-};
-BotNeckAPI.LoadConfig = function()
-{
-	if(!fs.existsSync(BotNeckData.botneckConfig))
-		return;
-
-	let conf = fs.readFileSync(BotNeckData.botneckConfig, "utf-8");
-	let cJson = JSON.parse(conf);
-
-	for(let key in cJson)
-	{
-		if(!BotNeckModules.hasOwnProperty(key) && key != "BotNeck")
-			continue;
-		if(key != "BotNeck")
-			if(typeof(BotNeckModules[key].settings) == "undefined")
-				continue;
-
-		for(let c in cJson[key])
-		{
-			if(key == "BotNeck")
-				BotNeckConfig[c] = cJson[key][c];
-			else
-				BotNeckModules[key].settings[c] = cJson[key][c];
-		}
-	}
-	BotNeckAPI.Log("Configuration loaded!");
-}
-BotNeckAPI.SaveConfig = function()
-{
-	if(fs.existsSync(BotNeckData.botneckConfig))
-		fs.unlinkSync(BotNeckData.botneckConfig);
-
-	let cfg = {};
-
-	cfg["BotNeck"] = BotNeckConfig;
-	for(let mod in BotNeckModules)
-		if(typeof(BotNeckModules[mod].settings) != "undefined")
-			cfg[mod] = BotNeckModules[mod].settings;
-	fs.writeFileSync(BotNeckData.botneckConfig, JSON.stringify(cfg))
-}
-
-BotNeckAPI.GetCurrentServerID = function()
-{
-	return window.location.pathname.split("/")[2];
-};
-BotNeckAPI.GetCurrentChannelID = function()
-{
-	return window.location.pathname.split("/")[3];
-};
-BotNeckAPI.GetDiscordToken = function() // WARNING: This uses an exploit in the iframe of electron. Use with caution!!
-{
-	let elem = document.createElement("iframe");
-	let token = document.body.appendChild(elem).contentWindow.localStorage.token.substring(1).slice(0, -1);
-
-	document.body.removeChild(elem);
-	return token;
-};
-BotNeckAPI.GetLastMessageID = function()
-{
-	return BotNeckData.LastMsgID;
-}
-
-BotNeckAPI.GetParameterValueFromText = function(text, parameterKey)
-{
-	let i = text.indexOf(parameterKey + "=");
-	if(i === -1)
-		return null;
-
-	let txt = text.substring(i + (parameterKey + "=").length);
-	let value = "";
-
-	if(txt.startsWith('"')) // Safe to assume it is a string
-	{
-		let ind = 1; // 1 to skip the "
-
-		while(ind < txt.length && txt[ind] != '"') // Loop until we hit the end of the text or until we hit a "
-		{
-			value += txt[ind];
-			ind++;
-		}
-	}
-	else // Not a string
-	{
-		let ind = 0; // Start from 0
-
-		while(ind < txt.length && txt[ind] != ' ') // Loop until we hit the end of the text or until we hit a space
-		{
-			value += txt[ind];
-			ind++;
-		}
+					// Push module
+					modules[makeKey()] = new BotNeckModule(sandbox);
+					console.log("Loaded module", name);
+				});
+			}
+		});
 	}
 
-	return value;
-};
-BotNeckAPI.GetParametersFromText = function(text)
-{
-	let params = {};
-  let name = "";
-  let value = "";
-  let onValue = false;
-  let isText = false;
+	start() {
+		// Setup overrides
+		XMLHttpRequest.prototype.setRequestHeader = function(header, value) {
+			if(header.toLowerCase() === "authorization")
+				protectedObject["token"] = value;
 
-  for(let i in text)
-  {
-    if(!onValue)
-    {
-      if(text[i] == "=")
-        onValue = true;
-      else
-        name += text[i];
-    }
-    else
-    {
-      if(text[i] == '"')
-        isText = !isText;
-      else if(text[i] == " " && !isText)
-      {
-        onValue = false;
-        isText = false;
+			setRequestHeader.call(this, [header, value]);
+		}
+		XMLHttpRequest.prototype.send = function(data) {
+			sendRequest.call(this, data);
+		}
+	}
+	stop() {
+		// Restore overrides
+		XMLHttpRequest.prototype.setRequestHeader = setRequestHeader;
+		XMLHttpRequest.prototype.send = sendRequest;
+	}
+}
+class BotNeckAPI {
+	static getCurrentServerId() { return window.location.pathname.split("/")[2]; }
+	static getCurrentChannelId() { return window.location.pathname.split("/")[3]; }
+	static getLastUserMessageId() { return protectedObject["lastUserMessageId"]; }
+	static getLastBotMessageId() { return protectedObject["lastBotMessageId"]; }
+}
+class BotNeckInternals {
+}
+class BotNeckSandBox {
+	constructor() {
+		this.name = null;
+		this.module = null;
+	}
 
-        params[name] = (isNaN(value) ? value : Number(value));
-        name = "";
-        value = "";
-      }
-      else
-      {
-        value += text[i];
-      }
-    }
-  }
-	if(value != "")
-  	params[name] = (isNaN(value) ? value : Number(value));
+	execute(code) {
+		try {
+			// Should prevent people from overriding stuff we don't want them to override
+			let XMLHttpRequest = XMLHttpRequest.clone();
+			let BotNeckAPI = BotNeckAPI.clone();
+			let API = BotNeckAPI;
 
-	return params;
+			eval(code);
+
+			// Check if valid
+			if(eval("typeof " + this.name) !== "function") {
+				console.error("The provided module name is undefined or invalid!", this.name);
+				return;
+			}
+			if(eval("typeof " + this.name + ".prototype.permissions") !== "array" ||
+			   eval("typeof " + this.name + ".prototype.execute") !== "function" ||
+			   eval("typeof " + this.name + ".prototype.command") !== "string") {
+				console.error("Invalid structure of", this.name);
+				return;
+			}
+
+			// Set data
+			this.module = eval(this.name + ".prototype");
+		} catch(err) {
+			console.error("Failed to load module", this.name);
+			console.error(err);
+		}
+	}
+}
+class BotNeckModule {
+	constructor(sandbox) {
+		this.sandbox = sandbox;
+		this.module = this.sandbox.module;
+		this.permissions = [...this.module.permissions];
+	}
 }
