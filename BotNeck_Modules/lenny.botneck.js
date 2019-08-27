9f0fe0694dd2387b0ca75be0ac5b3408e248596a
AtiLion <AtiLion@users.noreply.github.com>
2019-08-27 15:48:46 +0200

    Added lenny module for recode

---
+++ BotNeck_Modules/lenny.botneck.js
@@ -0,0 +1,12 @@
+class lenny {
+	constructor() {
+		this.permissions = [];
+		this.command = "lenny";
+		this.description = "Displays a lenny face";
+		this.usage = "lenny";
+	}
+
+	execute(message, args) {
+		message.content = "( ͡° ͜ʖ ͡°)";
+	}
+}
