0ff5f5c308269e46673ec1b77fe8bfc1e33df460
AtiLion <AtiLion@users.noreply.github.com>
2019-08-27 16:22:09 +0200

    Added love module for recode

---
+++ BotNeck_Modules/love.botneck.js
@@ -0,0 +1,40 @@
+class love {
+	constructor() {
+		this.permissions = [];
+		this.command = "love";
+		this.description = "Calculates the love between 2 users <3";
+		this.usage = "love <user1> [user2]";
+	}
+
+	execute(message, args) {
+		let user1 = args[0];
+		let user2 = args[1];
+		let txt = user1 + user2;
+		let process_num = "";
+		let processed_num = "";
+
+		process_num = (txt.match(/l/gi) || []).length.toString() + (txt.match(/o/gi) || []).length.toString() + (txt.match(/v/gi) || []).length.toString() + (txt.match(/e/gi) || []).length.toString() + (txt.match(/s/gi) || []).length.toString();
+		while(process_num.length > 2)
+		{
+			for(let i = 0; i < process_num.length - 1; i++)
+				processed_num += (Number(process_num[i]) + Number(process_num[i + 1])).toString();
+
+			process_num = processed_num;
+			processed_num = "";
+		}
+
+		message["embed"] = {
+			title: "Love Calculator",
+			type: "rich",
+			description: "How much does " + user1 + " love " + user2 + "?",
+			color: 0x0061ff,
+			fields: [
+				{
+					name: "Result",
+					value: process_num + "%",
+				},
+			]
+		};
+		delete message["content"];
+	}
+}
