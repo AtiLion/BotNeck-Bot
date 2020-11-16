9cebfb582a00e9b0fb03674db37cad4b67a05683
AtiLion <AtiLion@users.noreply.github.com>
2020-11-16 20:50:33 +0100

    Base presets

--- BotNeckBot/api/BotNeckPresets.js
+++ BotNeckBot/api/BotNeckPresets.js
@@ -1,4 +1,4 @@
-const { DiscordClientMessage } = require('./DiscordAPI');
+const { DiscordClientMessage, DiscordEmbed } = require('./DiscordAPI');
 
 module.exports = {
     /**
@@ -6,28 +6,30 @@ module.exports = {
      * @param {DiscordClientMessage} message The client message to turn into an error embed
      */
     createError: function(message, error) {
-        message.Content = '';
-        message.Embed = {
+        this.createBase(message, {
             Title: 'BotNeck Error',
             Description: error,
-            Color: 0xff6e00,
-            Author: {
-                Name: 'BotNeck Bot',
-                Url: 'https://github.com/AtiLion/BotNeck-Bot',
-                IconUrl: 'https://avatars1.githubusercontent.com/u/20825809?s=460&u=04546a97a5af1295a95eb1ff7a20eb9a9f76a80d&v=4'
-            },
-            Timestamp: new Date()
-        }
+            Color: 0xff6e00
+        });
     },
     /**
      * Changes a client message into an info embed
      * @param {DiscordClientMessage} message The client message to turn into an info embed
      */
     createInfo: function(message, info) {
+        this.createBase(message, {
+            Title: 'BotNeck Info',
+            Description: info
+        });
+    },
+    /**
+     * Changes a client message into the base embed
+     * @param {DiscordClientMessage} message The client message to turn into the base embed
+     * @param {DiscordEmbed} embed What information to add to the embed
+     */
+    createBase: function(message, embed) {
         message.Content = '';
         message.Embed = {
-            Title: 'BotNeck Info',
-            Description: info,
             Color: 0x0061ff,
             Author: {
                 Name: 'BotNeck Bot',
@@ -36,5 +38,8 @@ module.exports = {
             },
             Timestamp: new Date()
         }
+
+        for(let embedKey in embed)
+            message.Embed[embedKey] = embed[embedKey];
     }
 }
