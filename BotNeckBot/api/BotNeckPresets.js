d00d494d9d587c75f9d2e46d86f45c81bbb795e7
AtiLion <AtiLion@users.noreply.github.com>
2020-11-16 20:23:15 +0100

    Added timestamp to presets

--- BotNeckBot/api/BotNeckPresets.js
+++ BotNeckBot/api/BotNeckPresets.js
@@ -15,9 +15,14 @@ module.exports = {
                 Name: 'BotNeck Bot',
                 Url: 'https://github.com/AtiLion/BotNeck-Bot',
                 IconUrl: 'https://avatars1.githubusercontent.com/u/20825809?s=460&u=04546a97a5af1295a95eb1ff7a20eb9a9f76a80d&v=4'
-            }
+            },
+            Timestamp: new Date()
         }
     },
+    /**
+     * Changes a client message into an info embed
+     * @param {DiscordClientMessage} message The client message to turn into an info embed
+     */
     createInfo: function(message, info) {
         message.Content = '';
         message.Embed = {
@@ -28,7 +33,8 @@ module.exports = {
                 Name: 'BotNeck Bot',
                 Url: 'https://github.com/AtiLion/BotNeck-Bot',
                 IconUrl: 'https://avatars1.githubusercontent.com/u/20825809?s=460&u=04546a97a5af1295a95eb1ff7a20eb9a9f76a80d&v=4'
-            }
+            },
+            Timestamp: new Date()
         }
     }
 }
