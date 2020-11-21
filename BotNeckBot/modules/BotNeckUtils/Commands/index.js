3648f8eab76356a6364aaf1cfd2d26b242f9b5c5
AtiLion <AtiLion@users.noreply.github.com>
2020-11-21 11:32:33 +0100

    Added prefix command

--- BotNeckBot/modules/BotNeckUtils/Commands/index.js
+++ BotNeckBot/modules/BotNeckUtils/Commands/index.js
@@ -2,5 +2,6 @@ const Help = require('./Help');
 const Usage = require('./Usage');
 const Reload = require('./Reload');
 const ReloadConfig = require('./ReloadConfig');
+const Prefix = require('./Prefix');
 
-module.exports = { Help, Usage, Reload, ReloadConfig }
+module.exports = { Help, Usage, Reload, ReloadConfig, Prefix }
