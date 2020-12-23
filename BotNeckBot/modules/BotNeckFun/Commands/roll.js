524df3be5f3bf6cd1072250112d8186772796da7
AtiLion <AtiLion@users.noreply.github.com>
2020-12-23 22:37:38 +0100

    Added number of dice to roll

--- BotNeckBot/modules/BotNeckFun/Commands/roll.js
+++ BotNeckBot/modules/BotNeckFun/Commands/roll.js
@@ -9,8 +9,7 @@ const {
 module.exports = class RollCommand extends BotNeckCommand {
     get Command() { return 'roll'; }
     get Description() { return 'Rolls the dice'; }
-    get Usage() { return 'roll <max value>'; }
-    get MinimumArguments() { return 1; }
+    get Usage() { return 'roll [max value] dice=[number of dice to roll]'; }
 
     /**
      * This function gets executed when the command is called
@@ -18,13 +17,42 @@ module.exports = class RollCommand extends BotNeckCommand {
      * @param {any} args The arguments of the command
      */
     execute(message, args) {
-        if(isNaN(args[0]))
-            return BotNeckPresets.createError(message, 'Specified argument is not a number!');
+        let maxValue = 6;
+        if(args[0] && !isNaN(args[0]))
+            maxValue = Number(args[0]);
 
-        let num = Math.floor(Math.random() * Number(args[0]) + 1);
+        let rollAmount = 1;
+        if(args['dice'] && !isNaN(args['dice']))
+            rollAmount = Number(args['dice']);
+
+        let diceValues = [];
+        let sum = 0;
+        for(let i = 0; i < rollAmount; i++) {
+            let num = Math.floor(Math.random() * maxValue + 1);
+
+            diceValues.push(num);
+            sum += num;
+        }
         BotNeckPresets.createBase(message, {
             Title: 'Roll the dice',
-            Description: `The dice rolled a ${num} with the maximum value of ${args[0]}`
+            Description: `The dices have rolled: ${diceValues.join(', ')}`,
+            Fields: [
+                {
+                    Name: 'Sum of dice',
+                    Value: String(sum),
+                    Inline: true
+                },
+                {
+                    Name: 'Max roll value',
+                    Value: String(maxValue),
+                    Inline: true
+                },
+                {
+                    Name: 'Number of dice',
+                    Value: String(rollAmount),
+                    Inline: true
+                }
+            ]
         });
     }
 }
