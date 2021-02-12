5b0af5b9a9a6dc2894cd512298d53e26c01abfc2
AtiLion <AtiLion@users.noreply.github.com>
2021-02-12 21:22:59 +0100

    Fixed V2Command naming

--- BotNeckBot/api/v2Api/index.js
+++ BotNeckBot/api/v2Api/index.js
@@ -1,5 +1,5 @@
 const { getModulesPath } = require('./BotNeckAPI');
 const BotNeckAPI = require('./BotNeckAPI');
-const v2Command = require('./v2Command');
+const v2Command = require('./V2Command');
 
 module.exports = { BotNeckAPI, v2Command };
