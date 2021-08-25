6b9a1386c03e20bee6244c02f8a5f2b3c839603a
AtiLion <AtiLion@users.noreply.github.com>
2021-08-25 18:59:22 +0200

    Bumped version

--- BotNeckBot/core/BotNeckBot.js
+++ BotNeckBot/core/BotNeckBot.js
@@ -123,6 +123,6 @@ module.exports = class BotNeckBot {
 
     static get Name() { return 'BotNeck Bot'; }
     static get Description() { return 'Adds selfbot commands to the Discord client.'; }
-    static get Version() { return '3.0.3'; }
+    static get Version() { return '3.0.4'; }
     static get Author() { return 'AtiLion'; }
 }
